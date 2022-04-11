a35c8f318eb1f6a80bbc06387ee2758d4071adff
RadhaMaredi <99475599+RadhaMaredi@users.noreply.github.com>
2022-04-11 20:57:38 +0530

    disarium or not

--- python_folder/disarium.py
+++ python_folder/disarium.py
@@ -1,19 +1,23 @@
 #Create a function that determines whether a number is a Disarium or not?
 
-
-
 def find_disarium(input_a, convert_list, length_of_list):
 
     """this fun. finds the disarium number or not 
     and return the results."""
-    
-    count = 0
+
+    count = 0  # Initialize sum of terms
     for i in range(length_of_list):
+        #Calculates the sum of digits powered with their respective position 
         count += int(convert_list[i]) ** (i+1)
     count = str(count)
 
-    print ("Disarium Number") if count == input_a else  print ("Not a Disarium Number")
+    #Checks whether the sum is equal to the number itself   
+    if count == input_a:
+        print ("Disarium Number")
+    else:
+        print ("Not a Disarium Number")
 
+#declaring inputs
 input_a = input("Enter a number: ")
 convert_list = list(input_a)
 length_of_list = len(convert_list)
