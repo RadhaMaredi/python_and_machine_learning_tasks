405d4b6e6412f1bc14c59a41312d5217b05e599c
RadhaMaredi <99475599+RadhaMaredi@users.noreply.github.com>
2022-04-12 11:01:21 +0530

    client connection

--- python_folder/client1.py
+++ python_folder/client1.py
@@ -1,28 +1,31 @@
-
+#import the cocket
 import socket
 
 
 def client_program():
-    host = socket.gethostname()  # as both code is running on same pc
-    port = 5000  # socket server port number
 
-    client_socket = socket.socket()  # instantiate
+    """this function make a connection with the server by using the connection
+    and port number to communicate"""
+
+    host = socket.gethostname()  # as both code is running on same pc
+    port = 5000                  # socket server port number
+
+    client_socket = socket.socket()  # instantiate of socket
     client_socket.connect((host, port))  # connect to the server
 
-    message = input(" Enter client text -> ")  # take input
+    message = input(" Enter client text -> ")  # take input from the client side
 
     while message.lower().strip() != 'bye':
+        client_socket.send(message.encode())  # send message to server
+        data = client_socket.recv(1024).decode()  # receive response from the server
+        print('Received from server: ' + data)  # show the conversation in terminal
 
-        client_socket.send(message.encode())  # send message
+        # again take input from the client to further conversation or stop conversation
+        message = input("Enter client text -> ")  
 
-        data = client_socket.recv(1024).decode()  # receive response
+    client_socket.close()  # close the connection with server
 
-        print('Received from server: ' + data)  # show in terminal
-
-        message = input("Enter client text -> ")  # again take input
-
-    client_socket.close()  # close the connection
-
-
+#driver code
 if __name__ == '__main__':
+    #calling the function
     client_program()
