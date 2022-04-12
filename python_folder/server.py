1a0070028d71cbf84d80cd0277d28b4c5b4ce6c1
RadhaMaredi <99475599+RadhaMaredi@users.noreply.github.com>
2022-04-12 12:51:53 +0530

    client server

--- python_folder/server.py
+++ python_folder/server.py
@@ -1,13 +1,13 @@
-
-import socket
-
+import socket 
 
 def server_program():
-    # get the hostname
-    host = socket.gethostname()
-    port = 5000  # initiate port no above 1024
 
-    server_socket = socket.socket()  # get instance
+    """ this function make a connection with client to communication"""
+    
+    host = socket.gethostname() # get the hostname
+    port = 5000         # initiate port no above 1024
+
+    server_socket = socket.socket()  # get instance 
 
     # look closely. The bind() function takes tuple as argument
     server_socket.bind((host, port))  # bind host address and port together
@@ -24,12 +24,13 @@ def server_program():
         if not data:
             # if data is not received break
             break
+        #print the msg 
         print("from connected user: " + str(data))
         data = input(' Enter server text-> ')
         conn.send(data.encode())  # send data to the client
 
     conn.close()  # close the connection
 
-
+#driver code
 if __name__ == '__main__':
-    server_program()
+    server_program() #calling the server function
