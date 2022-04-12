187b72ba8d64a7f1aced40d521afb14a3d860840
RadhaMaredi <99475599+RadhaMaredi@users.noreply.github.com>
2022-04-12 11:55:00 +0530

    sending mail

--- python_folder/mail1.py
+++ python_folder/mail1.py
@@ -1,19 +1,23 @@
-import smtplib
+import smtplib  #import smtplib
 
-From ="radhamaredi1995@gmail.com"
-me = From
+#store from details in variables
+from_address ="radhamaredi1995@gmail.com"
+me = from_address
 pwd="yqozteemfxuiurzs"
 
+#store the sending address in variables
 to=["vibha.rawan@neosoftmail.com"]
 cc =["lakshmi73862@gmail.com", "grandheraj68@gmail.com"]
 bcc =["arun.nadar@neosoftmial.com","rohan.dhere@neosoftmail.com"]
 
+#concatinate all sending address
 receiver_mail= cc + to + bcc
 
-
+#store subject and body of our mail in variables
 text="Hi ! this is Radha Maredi"
 sub="Send mail through python code"
 
+#join the from, sending address and subject, body 
 email_text = """\
 From: %s
 To: %s
@@ -24,12 +28,11 @@ cc: %s
 
 #hosting services, choose which server to sending mail,port
 server =smtplib.SMTP('smtp.gmail.com',587)  
-server.starttls() # security purpose
+server.starttls()       #used this for security purpose
+server.login(from_address, pwd)  #login to the mail
+#sending the mail
+server.sendmail(from_address, receiver_mail, email_text)
+server.quit() #quit from the mail
 
-server.login(From, pwd)
-
-server.sendmail(From, receiver_mail, email_text)
-
-server.quit() #quit
-
+#print the text to know weather our text was successfullly sent or not.
 print("successfully sent")
