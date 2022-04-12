330cbb25c8b392989df0f07ad505f94aaaae6332
RadhaMaredi <99475599+RadhaMaredi@users.noreply.github.com>
2022-04-12 12:55:04 +0530

    remove special symbol using regex

--- python_folder/string_regex.py
+++ python_folder/string_regex.py
@@ -1,6 +1,11 @@
 #Q. remove the special characters from given string?
-import re  #rejex used to remove special symbols
+
+import re  #regex used to remove special symbols
 
 string_a ="This#string%contains^special*characters&."
+
+#removing the special characters from the string
 result = re.sub("[#%^*&.]", " ", string_a)
+
+#print the result
 print(result)
