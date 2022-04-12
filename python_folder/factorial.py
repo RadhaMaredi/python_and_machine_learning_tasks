24021af004bfccac68e4caa9494bf6bacf345245
RadhaMaredi <99475599+RadhaMaredi@users.noreply.github.com>
2022-04-12 11:17:16 +0530

    factorial

--- python_folder/factorial.py
+++ python_folder/factorial.py
@@ -1,16 +1,31 @@
-#find the factorial of the factorial
+#Q.find the factorial of the factorial 
 
-#fact fun does the factorial of each value in factorial and return it
-def fact(num):
-    return 1 if num == 0 else num * fact(num-1)
 
-#subfact fun. does the multiplication of each factorial value and return it
-def subfact(num):
-    n = 1
+def factorial_num(num):
+
+    """this factorial_num takes input as the factorial of each 
+    value in factorial and return the value"""
+    
+    #here i used ternary operator. if num equal to zero it returns 1 
+    #else it perform the recursive by callin it self.
+    return 1 if num == 0 else num * factorial_num(num-1)
+
+def sub_factorial(num):
+
+    """subfact fun. takes the number as input and does 
+    the multiplication of each factorial value and return it"""
+    
+    #declare a variable with value 1. because our value should not 
+    # have zero to perform factoial.
+    n = 1 
     while num != 0:
-        n = fact(num) * n
+        #here we are calling the factorial_nm function to perform factorial
+        n = factorial_num(num) * n
         num = num -1
     return n
 
+#takes input from the user
 num = int(input("Enter the number to know factorial: "))
-print("The factorial of", num, "is: ", subfact(num))
+
+#print the output and calling the sub_factorial function
+print("The factorial of", num, "is: ", sub_factorial(num))
