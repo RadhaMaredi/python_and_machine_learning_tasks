632256c044c0f11e01a6a041d396bd00ec0baea7
RadhaMaredi <99475599+RadhaMaredi@users.noreply.github.com>
2022-04-12 10:34:20 +0530

    crate the arithmatic fun.

--- python_folder/arithmatic.py
+++ python_folder/arithmatic.py
@@ -1,11 +1,16 @@
-def add (num1, num2) :
+#declaring the arithmatic aperation functions
+def add(num1, num2) :
+    """take two numbers and returns the addition"""
     return num1 + num2
 
 def sub(num1, num2):
+    """take two numbers and returns the subtraction"""    
     return num1 - num2
 
 def mul(num1, num2) :
+    """take two numbers and returns the multiplication"""
     return num1 * num2
 
 def div(num1, num2):
+    """take two numbers and returns the division"""
     return num1 / num2
