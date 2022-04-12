31de92f1cc1c8cc3ac3ec5847199c75acbdf3a52
RadhaMaredi <99475599+RadhaMaredi@users.noreply.github.com>
2022-04-12 12:12:05 +0530

    longest substring

--- python_folder/longest_substr.py
+++ python_folder/longest_substr.py
@@ -6,14 +6,21 @@ class Solution:
     def lengthOfLongestSubstring(self, str):
         """it takes the string as an input, finds the longest substring and
         return the number of longest sub strings"""
-
-        sub = {}
+        
+        #creating an empty dictionary to store 
+        #the each character how many time  repeation.
+        sub = {} 
         current_substing_start = 0
         current_len = 0
         longest = 0
         
         for i, letter in enumerate(str):
+            #this loop iterate over the string and 
+            # also it gives the count because we used enumerate.
+
             if (letter in sub) and (sub[letter] >= current_substing_start):
+                #this block gives us no. of times a char. repeated in string and stored in dictionary 
+                #as key:value format, here key means character and value is count of the character 
                 current_substing_start = sub[letter] + 1
                 current_len = i - sub[letter]
                 sub[letter] = i
@@ -21,12 +28,15 @@ class Solution:
             else:
                 sub[letter] = i
                 current_len += 1
+                #this if block give the longest substring
                 if current_len > longest:
                     longest = current_len
         return longest
 
-obj = Solution()
-my_string = input()
+obj = Solution()    #creating the object
+my_string = input() #taking input
+
+#calling the method by passing my_string as argument
 result = obj.lengthOfLongestSubstring(my_string)
-print(result)
+print(result)  #print the result
                 
