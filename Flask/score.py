50012ac1fd04a7447b553cd47a4ea1773fa521d7
RadhaMaredi <99475599+RadhaMaredi@users.noreply.github.com>
2022-04-15 13:01:51 +0530

    marks score task

--- Flask/score.py
+++ Flask/score.py
@@ -7,40 +7,52 @@
 {{    }} expressions to print output
 {#....#} this is for comments
 '''
+
 from flask import Flask,redirect,url_for,render_template,request
 
 app=Flask(__name__)
 
 @app.route('/')
 def welcome():
+
     return render_template('index.html')
 
 @app.route('/success/<int:score>')
 def success(score):
+
+    """ take the avg score and return to the result if he pass or fail """
+    
     res=""
+    #decide the result
     if score>=50:
         res="PASS"
+
     else:
         res='FAIL'
+
     exp={'score':score,'res':res}
-    return render_template('result.html',result=exp) #sending result to result.html
+    #sending result to result.html
+    return render_template('result.html',result=exp) 
 
-
-### Result checker submit html page
+#Result checker submit html page
 @app.route('/submit',methods=['POST','GET'])
 def submit():
+
+    """getting details from the user and return the
+    avg score to success method"""
+
     total_score=0
+
     if request.method=='POST':
-        python=int(request.form['py']) #getting values from the user by using name value from index.html
+        #getting values from the user by using name value from index.html
+        python=int(request.form['py']) 
         english=int(request.form['eng'])
-        
+    
         total_score=(python+english)/2
 
-    return redirect(url_for('success',score=total_score)) #redirect to success method
+    #redirect to success method
+    return redirect(url_for('success',score=total_score)) 
 
-    
-
-
-
+#driver code
 if __name__=='__main__':
     app.run(debug=True)
