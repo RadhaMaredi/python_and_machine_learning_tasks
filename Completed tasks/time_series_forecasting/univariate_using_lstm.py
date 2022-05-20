686641f049a5d5634706b7fcbb19499104a11056
RadhaMaredi <99475599+RadhaMaredi@users.noreply.github.com>
2022-05-20 11:27:03 +0530

    uni variate using lstm

---
+++ Completed tasks/time_series_forecasting/univariate_using_lstm.py
@@ -0,0 +1,131 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM
+from tensorflow.keras.layers import Dense, Dropout
+import pandas as pd
+from matplotlib import pyplot as plt
+from sklearn.preprocessing import StandardScaler
+import seaborn as sns
+
+#from datetime import datetime
+#Read the csv file
+df = pd.read_csv('/home/neosoft/Downloads/GE.csv')
+df.head()
+
+#Separate dates for future plotting
+train_dates = pd.to_datetime(df['Date'])
+train_dates.tail(15)
+
+#Variables for training
+cols = list(df)[1:6]
+#Date and volume columns are not used in training. 
+cols #['Open', 'High', 'Low', 'Close', 'Adj Close']
+
+#New dataframe with only training data - 5 columns
+df_for_training = df[cols].astype(float)
+df_for_training
+
+#visualize the dataset
+df_for_plot=df_for_training.tail(5000)
+df_for_plot.plot.line()
+
+#LSTM uses sigmoid and tanh that are sensitive to magnitude so values need to be normalized
+# normalize the dataset
+scaler = StandardScaler()
+scaler = scaler.fit(df_for_training)
+df_for_training_scaled = scaler.transform(df_for_training)
+
+#As required for LSTM networks, we require to reshape an input data into n_samples x timesteps x n_features. 
+#In this example, the n_features is 5. We will make timesteps = 14 (past days data used for training). 
+
+#Empty lists to be populated using formatted training data
+trainX = []
+trainY = []
+
+n_future = 1   # Number of days we want to look into the future based on the past days.
+n_past = 14   #Number of past days we want to use to predict the future.
+
+#Reformat input data into a shape: (n_samples x timesteps x n_features)
+#In my example, my df_for_training_scaled has a shape (12823, 5)
+#12823 refers to the number of data points and 5 refers to the columns (multi-variables).
+
+for i in range(n_past, len(df_for_training_scaled) - n_future +1):
+    trainX.append(df_for_training_scaled[i - n_past:i, 0:df_for_training.shape[1]])
+    trainY.append(df_for_training_scaled[i + n_future - 1:i + n_future, 0])
+
+trainX, trainY = np.array(trainX), np.array(trainY)
+
+print('trainX shape == {}.'.format(trainX.shape))
+print('trainY shape == {}.'.format(trainY.shape))
+
+#In my case, trainX has a shape (12809, 14, 5). 
+#12809 because we are looking back 14 days (12823 - 14 = 12809). 
+#Remember that we cannot look back 14 days until we get to the 15th day. 
+#Also, trainY has a shape (12809, 1). Our model only predicts a single value, but 
+#it needs multiple variables (5 in my example) to make this prediction. 
+#This is why we can only predict a single day after our training, the day after where our data ends.
+#To predict more days in future, we need all the 5 variables which we do not have. 
+#We need to predict all variables if we want to do that. 
+
+# define the Autoencoder model
+
+model = Sequential()
+model.add(LSTM(64, activation='relu', input_shape=(trainX.shape[1], trainX.shape[2]), return_sequences=True))
+model.add(LSTM(32, activation='relu', return_sequences=False))
+model.add(Dropout(0.2))
+model.add(Dense(trainY.shape[1]))
+model.compile(optimizer='adam', loss='mse')
+model.summary()
+
+# fit the model
+history = model.fit(trainX, trainY, epochs=5, batch_size=16, validation_split=0.1, verbose=1)
+
+#plot the train and validation losses
+plt.plot(history.history['loss'], label='Training loss')
+plt.plot(history.history['val_loss'], label='Validation loss')
+plt.legend()
+
+#Predicting...
+#Libraries that will help us extract only business days in the US.
+#Otherwise our dates would be wrong when we look back (or forward).
+
+from pandas.tseries.holiday import USFederalHolidayCalendar
+from pandas.tseries.offsets import CustomBusinessDay
+us_bd = CustomBusinessDay(calendar=USFederalHolidayCalendar())
+
+#Remember that we can only predict one day in future as our model needs 5 variables
+#as inputs for prediction. We only have all 5 variables until the last day in our dataset.
+n_past = 16
+n_days_for_prediction=15  #let us predict past 15 days
+
+predict_period_dates = pd.date_range(list(train_dates)[-n_past], periods=n_days_for_prediction, freq=us_bd).tolist()
+predict_period_dates
+
+#Make prediction
+prediction = model.predict(trainX[-n_days_for_prediction:]) #shape = (n, 1) where n is the n_days_for_prediction
+prediction
+
+#Perform inverse transformation to rescale back to original range
+#Since we used 5 variables for transform, the inverse expects same dimensions
+#Therefore, let us copy our values 5 times and discard them after inverse transform
+prediction_copies = np.repeat(prediction, df_for_training.shape[1], axis=-1)
+y_pred_future = scaler.inverse_transform(prediction_copies)[:,0]
+y_pred_future
+
+# Convert timestamp to date
+forecast_dates = []
+for time_i in predict_period_dates:
+    forecast_dates.append(time_i.date())
+
+df_forecast = pd.DataFrame({'Date':np.array(forecast_dates), 'Open':y_pred_future})
+df_forecast['Date']=pd.to_datetime(df_forecast['Date'])
+df_forecast
+
+original = df[['Date', 'Open']]
+original['Date']=pd.to_datetime(original['Date'])
+original = original.loc[original['Date'] >= '2020-5-1']
+original
+
+sns.lineplot(original['Date'], original['Open'])
+sns.lineplot(df_forecast['Date'], df_forecast['Open'])
+    
